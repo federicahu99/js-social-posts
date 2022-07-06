c61be28a57cff2d9b49a7482d455bc9796a97031
feddericahu99 <federica.hu339@gmail.com>
2022-07-06 18:37:25 +0200

    almost milestone 3

--- js/script.js
+++ js/script.js
@@ -17,16 +17,32 @@
     Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 */
 
+// funzioni 
+function likeAPicture() {
+    newLike = infos.likes++;
+    infos.push(newLike);
+}
+
 // creo array  
 const infos = [
     {
     idNumber: 568, 
     author: 'Kevin Smith', 
-    photo: 'https://unsplash.it/300/300?image=15', 
+    picture: 'https://picsum.photos/200/200/?blur',
+    photo: 'https://picsum.photos/seed/picsum/600/300', 
     date: '12-07-2021', 
     caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia cum nam harum, minima expedita inventore...', 
     likes: 51
-}
+    }, 
+    {
+    idNumber: 553, 
+    author: 'Lauren Green', 
+    picture: 'https://picsum.photos/200/200/?blur',
+    photo: 'https://picsum.photos/seed/picsum/500/300', 
+    date: '09-07-2021', 
+    caption: 'Quia cum nam harum, minima expedita inventore...', 
+    likes: 198
+    }, 
 ]
 
 // recupero dal dom
@@ -34,23 +50,33 @@ const container = document.getElementById('container');
 
 let post = '';
 
-for (i=0 ; i < infos.length; i++) {
+for (i=0 ; i < infos.length -1; i++) {
     post += 
-    `<div class="header-card">
-     <img src="" alt="" class="profile-picture">
-     <div class="author"> ${infos[i]['author']} </div>
-     <div class="date"> ${[infos[i]['date']]}</div>
-     </div>
-     <div class="center-card">
-     <figure>
-       <img src="${infos[i]['photo']}" alt="photo${i}" class="post-img">
-     </figure>
-     <div class="caption">${[infos[i]['caption']]}</div>
-     </div>
-     <div class="footer-card">
-     <button> Mi piace </button>
-     <div class='likes'> Likes: ${[infos[i]['likes']]}</div>
-     </div>`;
+    `<div class="card">
+        <div class="header-card">
+            <img src="${infos[i]['picture']}" alt="photo${i}" class="profile-picture">
+            <div class="author"> ${infos[i]['author']}</div>
+            <div class="date"> ${[infos[i]['date']]}</div>
+        </div>
+            <div class="caption">${[infos[i]['caption']]}</div>
+        <div class="center-card">
+            <figure>
+                <img src="${infos[i]['photo']}" alt="photo${i}" class="post-img">
+            </figure>
+        </div>
+        <div class="footer-card">
+            <button id="btn"> Mi piace </button>
+            <div class='likes'> Likes: ${[infos[i]['likes']]}</div>
+        </div>
+    </div>`;
     container.innerHTML += post;
 }
+
+// evento
+const btn = document.getElementById('btn');
+btn.addEventListener('click', (event) => {
+    event.target.classList.toggle('liked');
+    event.target.innerText = 'Piaciuto';
+    likeAPicture()
+})
 
