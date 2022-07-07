f1fbd862d201b723dd41998b47aa64a322f23fad
feddericahu99 <federica.hu339@gmail.com>
2022-07-07 10:09:57 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 /*
  Milestone 1
     Creiamo il nostro array di oggetti che rappresentano ciascun post.
@@ -28,6 +29,7 @@ const infos = [
     {
     idNumber: 568, 
     author: 'Kevin Smith', 
+    photo: 'https://unsplash.it/300/300?image=15', 
     picture: 'https://picsum.photos/200/200/?blur',
     photo: 'https://picsum.photos/seed/picsum/600/300', 
     date: '12-07-2021', 
@@ -42,7 +44,7 @@ const infos = [
     date: '09-07-2021', 
     caption: 'Quia cum nam harum, minima expedita inventore...', 
     likes: 198
-    }, 
+    }
 ]
 
 // recupero dal dom
@@ -50,25 +52,41 @@ const container = document.getElementById('container');
 
 let post = '';
 
-for (i=0 ; i < infos.length -1; i++) {
+for (i=0 ; i <= infos.length; i++) {
     post += 
-    `<div class="card">
-        <div class="header-card">
-            <img src="${infos[i]['picture']}" alt="photo${i}" class="profile-picture">
-            <div class="author"> ${infos[i]['author']}</div>
-            <div class="date"> ${[infos[i]['date']]}</div>
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${[infos[i]['picture']]}" alt="${[infos[i]['picture']]}" />
+                </div>
+                <div class="post-meta__data">
+                <div class="post-meta__author">${[infos[i]['author']]}</div>
+                <div class="post-meta__time">${[infos[i]['date']]}</div>
+            </div>
         </div>
-            <div class="caption">${[infos[i]['caption']]}</div>
-        <div class="center-card">
-            <figure>
-                <img src="${infos[i]['photo']}" alt="photo${i}" class="post-img">
-            </figure>
+    </div>
+
+    <div class="post__text">
+      Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint
+      voluptate recusandae architecto. Et nihil ullam aut alias.
+    </div>
+    <div class="post__image">
+      <img src="${[infos[i]['photo']]}" alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="1">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
         </div>
-        <div class="footer-card">
-            <button id="btn"> Mi piace </button>
-            <div class='likes'> Likes: ${[infos[i]['likes']]}</div>
-        </div>
-    </div>`;
+        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone</div>
+      </div>
+    </div>
+  </div>
+</div>`;
     container.innerHTML += post;
 }
 
@@ -79,4 +97,3 @@ btn.addEventListener('click', (event) => {
     event.target.innerText = 'Piaciuto';
     likeAPicture()
 })
-
